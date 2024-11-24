ae69d66173659f968324c2eb3e81040a844562f4
Stanislaw <southerland1337@gmail.com>
2024-11-24 17:06:31 +0300

    Add logging using log/slog

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"grpc-auth/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("Log init.")
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug, AddSource: true}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+
+	}
+	return log
 }
