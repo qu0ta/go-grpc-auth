5c10fbfbc2deaaac52fc0e6cee46ab1c0e0c3888
Stanislaw <southerland1337@gmail.com>
2024-11-24 22:08:24 +0300

    Add request validating

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/qu0ta/go-grpc-auth/internal/config"
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 const (
@@ -21,7 +23,18 @@ func main() {
 	log.Info("Starting gRPC server")
 
 	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
-	application.GRPCServer.MustRun()
+	go application.GRPCServer.MustRun()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-stop
+
+	log.Info("Shutting down gRPC server", "signal", sig)
+
+	application.GRPCServer.Stop()
+
+	log.Info("gRPC server stopped")
 }
 
 func setupLogger(env string) *slog.Logger {
--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -4,24 +4,77 @@ import (
 	"context"
 	authv1 "github.com/qu0ta/pet-proto/gen/go/auth"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
+type Auth interface {
+	Login(ctx context.Context, email string, password string) (token string, err error)
+	RegisterUser(ctx context.Context, email string, password string) (userID int64, err error)
+	IsAdmin(ctx context.Context, userID int64) (isAdmin bool, err error)
+}
 type serverAPI struct {
 	authv1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server) {
-	authv1.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	authv1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
 
 func (s *serverAPI) Login(ctx context.Context, req *authv1.LoginRequest) (*authv1.LoginResponse, error) {
-	panic("unimplemented")
+	if err := validateLogin(req); err != nil {
+		return nil, err
+
+	}
+
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Internal error")
+	}
+
+	return &authv1.LoginResponse{Token: token}, nil
 }
 
 func (s *serverAPI) Register(ctx context.Context, req *authv1.RegisterRequest) (*authv1.RegisterResponse, error) {
-	panic("unimplemented")
+	if err := validateRegister(req); err != nil {
+		return nil, err
+	}
+
+	userId, err := s.auth.RegisterUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Internal error")
+	}
+
+	return &authv1.RegisterResponse{UserId: userId}, nil
 }
 
 func (s *serverAPI) IsAdmin(ctx context.Context, req *authv1.IsAdminRequest) (*authv1.IsAdminResponse, error) {
-	panic("unimplemented")
+	if err := validateIsAdmin(req); err != nil {
+		return nil, err
+	}
+
+	isAdmin, err := s.auth.IsAdmin(ctx, req.UserId)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Internal error")
+	}
+	return &authv1.IsAdminResponse{IsAdmin: isAdmin}, nil
+}
+func validateLogin(req *authv1.LoginRequest) error {
+	if req.GetEmail() == "" || req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "Invalid argument")
+	}
+	return nil
+}
+func validateRegister(req *authv1.RegisterRequest) error {
+	if req.GetEmail() == "" || req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "Invalid argument")
+	}
+	return nil
+}
+func validateIsAdmin(req *authv1.IsAdminRequest) error {
+	if req.UserId == 0 {
+		return status.Error(codes.InvalidArgument, "Invalid argument")
+	}
+	return nil
 }
