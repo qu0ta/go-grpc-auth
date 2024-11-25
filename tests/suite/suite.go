7ba6509b64639d880976425dfa4f8e5b2f1899e9
Stanislaw <southerland1337@gmail.com>
2024-11-25 23:53:44 +0300

    complete grpc client init

--- tests/suite/suite.go
+++ tests/suite/suite.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"github.com/qu0ta/go-grpc-auth/internal/config"
 	authv1 "github.com/qu0ta/pet-proto/gen/go/auth"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"testing"
 )
 
@@ -16,5 +18,26 @@ type Suite struct {
 func New(t *testing.T) (context.Context, *Suite) {
 	t.Helper()
 	t.Parallel()
+
+	cfg := config.MustLoadByPath("../config/config.test.yaml")
+
+	ctx, cancelCtx := context.WithTimeout(context.Background(), cfg.GRPC.Timeout)
+
+	t.Cleanup(func() {
+		t.Helper()
+		cancelCtx()
+	})
+
+	cc, err := grpc.DialContext(ctx, "localhost:50000",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return ctx, &Suite{
+		T:          t,
+		Cfg:        cfg,
+		AuthClient: authv1.NewAuthClient(cc),
+	}
 
 }
